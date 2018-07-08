3bd6b1a3badfa949161829c91b314a51f119f5b8
Ricardo Garcia <Richard_Grac@hotmail.com>
2018-07-08 14:05:22 -0500

    +Listado de Clientes y Peliculas. +Detalles de Cliente.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private List<Customer> customers = new List<Customer>
+        {
+            new Customer() {Id = 1, Name = "Richard Grac"},
+            new Customer() {Id = 2, Name = "Mauricio Romo"},
+            new Customer() {Id = 3, Name = "Josue Gonzalez"},
+            new Customer() {Id = 4, Name = "Daniel Pedroza"}
+
+        };
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = customers.Find(x => x.Id == id);
+            return View(customer);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,6 +11,19 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        List<Movie> movies = new List<Movie>
+        {
+            new Movie() {Id = 1, Name = "Batman Begins"},
+            new Movie() {Id = 2, Name = "Batman The Dark Knight"},
+            new Movie() {Id = 3, Name = "Batman The Dark Knight Rises"}
+        };
+
+        public ActionResult Index()
+        {
+            return View(movies);
+        }
+
+
         // GET: Movies/Random
         //public ActionResult Random()
         //{
@@ -23,51 +36,51 @@ namespace Vidly.Controllers
         //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
         //}
 
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Batman Begins" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1"},
-                new Customer { Name = "Customer 2"}
-            };
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Batman Begins" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name = "Customer 1"},
+        //        new Customer { Name = "Customer 2"}
+        //    };
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-            return View(viewModel);
-        }
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+        //    return View(viewModel);
+        //}
 
-        // localhost:65118/Movies/Edit?id=10
-        // localhost:65118/Movies/Edit/10
-        public ActionResult Edit(int id)
-        {
-            return Content("id = " + id);
-        }
+        //// localhost:65118/Movies/Edit?id=10
+        //// localhost:65118/Movies/Edit/10
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id = " + id);
+        //}
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //    {
+        //        pageIndex = 1;
+        //    }
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        sortBy = "Name";
+        //    }
 
-            return Content(String.Format("pageIndex = {0} \n sortBy = {1}", pageIndex, sortBy));
-        }
+        //    return Content(String.Format("pageIndex = {0} \n sortBy = {1}", pageIndex, sortBy));
+        //}
 
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
 
-            return Content(year + "/" + month);
-        }
+        //    return Content(year + "/" + month);
+        //}
 
     }
 }
