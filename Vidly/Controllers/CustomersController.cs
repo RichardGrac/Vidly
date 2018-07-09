398f4058b56c662ff2207d901c1939e95879514d
Ricardo Garcia <Richard_Grac@hotmail.com>
2018-07-08 22:16:08 -0500

    DB Up with Entity Framework. Customers with more attributes.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;   // Para la carga 'Eager' de MembershipType
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,24 +10,42 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        private List<Customer> customers = new List<Customer>
-        {
-            new Customer() {Id = 1, Name = "Richard Grac"},
-            new Customer() {Id = 2, Name = "Mauricio Romo"},
-            new Customer() {Id = 3, Name = "Josue Gonzalez"},
-            new Customer() {Id = 4, Name = "Daniel Pedroza"}
+        private List<Customer> customers = new List<Customer>();
+        private ApplicationDbContext _context;
 
-        };
-
-        // GET: Customers
-        public ActionResult Index()
+        public CustomersController()
         {
-            return View(customers);
+            _context = new ApplicationDbContext();
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: localhost:5000/Customers
+        public ActionResult Index()
+        {
+            getCustomers();
+            return View(this.customers);
+        }
+
+        /* Returns the list of Customers from DB */
+        private void getCustomers()
+        {
+            this.customers = _context.Customers.Include(c => c.MembershipType).ToList();
+        }
+
+
+        // GET: localhost:5000/Customers/{id}
         public ActionResult Details(int id)
         {
-            var customer = customers.Find(x => x.Id == id);
+            //var customer = customers.Find(x => x.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
             return View(customer);
         }
     }
